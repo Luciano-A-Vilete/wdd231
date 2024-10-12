84cbcfc546abb11b2842d37f1fede9e0becfe691
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2024-10-12 10:26:46 -0300

    Dialog addition

--- scripts/home.js
+++ scripts/home.js
@@ -142,6 +142,10 @@ function displayCourses(courseArray) {
         //append to courses section
         courseSection.appendChild(cName);
     });
+
+    courseDiv.addEventListener('click', () => {
+        DisplayCoursesDetails(courses);
+    });
 }
 
 // add event listeners for course selection buttons
@@ -170,7 +174,23 @@ WDDButton.addEventListener('click', function() {
     });
 });
  
+function DisplayCoursesDetails(course){
+    courseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+        <button id="closeModal">X</button>
+        <h2>${course.subject} ${course.number}</h2>
+        <h3>${course.title}</h3>
+        <p><strong>Credits</strong>: ${course.credits}</p>
+        <p><strong>Certificate</strong>: ${course.certificate}</p>
+        <p>${course.description}</p>
+        <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+    `;
+    courseDetails.showModal();
 
+    closeModal.addEventListener("click", () => {
+        couseDetails.close();
+    });
+}
 /*
 allButton.addEventListener('click', function() {
     courses.forEach(course => {
